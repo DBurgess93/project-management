f7b9c4dc7ecf311de564b02bbf009b72e63ca3e4
Daniel Burgess <dan-burgess@hotmail.com>
2023-01-24 00:02:24 +1030

    add Project percentage and tasks complete

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,12 +14,29 @@ class Project < ApplicationRecord
   end
 
   def status
+    return 'not-started' if tasks.none?
     if tasks.all?(&:complete?)
       'complete'
-    elsif tasks.any?(&:in_progress?)
+    elsif tasks.any?(&:in_progress?) || tasks.any?(&:complete?)
       'in-progress'
     else
       'not-started'
     end
+  end
+
+  def percent_complete
+    return 0 if tasks.none?
+
+    # original way shown in tutorial - replaced instead by total_complete method
+    # complete_tasks = tasks.select(&:complete?).count
+    ((total_complete.to_f / tasks.count) * 100).round
+  end
+
+  def total_complete
+    tasks.select(&:complete?).count
+  end
+
+  def total_tasks
+    tasks.count
   end
 end
