b11130fb7ae2d0facb7de1fa4304dfc7174fc75b
Daniel Burgess <dan-burgess@hotmail.com>
2023-01-23 23:39:30 +1030

    add methods for project status

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,25 @@
 class Project < ApplicationRecord
   has_many :tasks
   belongs_to :user
+
+  def badge_color
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+
+  def status
+    if tasks.all?(&:complete?)
+      'complete'
+    elsif tasks.any?(&:in_progress?)
+      'in-progress'
+    else
+      'not-started'
+    end
+  end
 end
